c080c933fb4fce0aae330e1d83abd997083bf6f9
Burak Furkan Asilturk <furkan.asilturk@gmail.com>
2020-09-24 21:31:36 +0300

    Initial Commit

---
+++ Momento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Momento
+//
+//  Created by Burak Furkan Asilturk on 24.09.2020.
+//  Copyright © 2020 Burak Furkan Asilturk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
