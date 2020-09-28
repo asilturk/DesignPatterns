20e85f6574764386e0964531e26bb090760867cf
Burak Furkan Asilturk <furkan.asilturk@gmail.com>
2020-09-28 23:45:31 +0300

    prototype

---
+++ Prototype/Prototype/Prototype.swift
@@ -0,0 +1,58 @@
+//
+//  Prototype.swift
+//  Prototype
+//
+//  Created by Burak Furkan Asilturk on 28.09.2020.
+//
+
+import UIKit
+
+class Car {
+    var power: Int
+    var color: UIColor?
+
+    init(power: Int, color: UIColor? = nil) {
+        self.power = power
+    }
+}
+
+class Person {
+    var id: Int
+    var name: String
+    var car: Car
+
+    init(id: Int, name: String, car: Car) {
+        self.id = id
+        self.name = name
+        self.car = car
+    }
+}
+
+extension Person: NSCopying {
+    func copy(with zone: NSZone? = nil) -> Any {
+        // shallow copy
+        return Person(id: id, name: name, car: car)
+
+        // deep copy
+        return Person(id: id, name: name, car: car.copy() as! Car)
+    }
+}
+
+extension Car: NSCopying {
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Car(power: power) as! Car
+    }
+}
+
+extension Person: CustomStringConvertible {
+    var description: String {
+        "personId: \(id), name: \(name), car: \(car.description)"
+    }
+}
+
+extension Car: CustomStringConvertible {
+    var description: String {
+        "horse power: \(power)"
+    }
+}
+
---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Burak Furkan Asilturk on 28.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let car = Car(power: 99, color: .red)
+        let burak = Person(id: 0, name: "Burak", car: car)
+
+        let furkan = burak.copy() as! Person
+        furkan.name = "Furkan"
+        furkan.id = 1
+        furkan.car.power = 101
+
+        print("person1: \(burak.description)")
+        print("person2: \(furkan.description)")
+    }
+
+
+}
+
